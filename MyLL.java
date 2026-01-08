99791d90ebd70ddad336512910872deb593478ac
purpleisgreen <83792294+purpleisgreen@users.noreply.github.com>
2026-01-07 23:30:23 -0500

    Update MyLL.java

--- MyLL.java
+++ MyLL.java
@@ -1,46 +1,106 @@
 
 public class MyLL {
 	
-	// The list manager class!
-	
 	public StudentInfo front;
+	public StudentInfo rear;
 	public int num;
 	
 	public MyLL() {
 		front = null;
+		rear = null;
 		num = 0;
 	}
 	
-	
 	public void addToFront(StudentInfo studentToAdd) {
-		
 		if (num == 0) {
 			front = studentToAdd;
-			front.next = null; // just to be safe!
+			rear = studentToAdd;	
+			studentToAdd.next = null;
 			num++;
 			return;
-		}
-		
+		}	
 		if (num > 0) {
 			studentToAdd.next = front;
 			front = studentToAdd;
 			num++;
 			return;
+		}	
+	}
+
+	//Add to back in O(1) with rear pointer
+	public void addToRear(StudentInfo studentToAdd){
+		if (num == 0) {
+			front = studentToAdd;
+			rear = studentToAdd;
+			studentToAdd.next = null;
+			num++;
+			return;
 		}
-	
-		
+		if (num > 0){
+			rear.next = studentToAdd;
+			rear = studentToAdd;
+			rear.next = null;
+			num++;
+		}
+	}
+
+	//Remove from a certain index, returns removed object, or null if invalid
+	public StudentInfo removeAtIndex(int index){
+		if (index < 0 || index >= num){
+			return null;
+		}
+		if (index == 0){
+			//Remove from front
+			StudentInfo removed = front;
+			front = front.next;
+			if (front == null){
+				rear = null; //now an empty list
+			}
+			num--;
+			return removed;
+		}
+		StudentInfo current = front;
+		for (int i = 0; i < index -1; i++){
+			current = current.next;
+		}
+		StudentInfo removed = current.next;
+		current.next = removed.next;
+
+		if (index == num-1){
+			rear = current; //update rear if the old rear was removed
+		}
+		num--;
+		return removed;
+	}
+
+	//Add at a certain index
+	public void addAtIndex(int index, StudentInfo studentToAdd){
+		if (index < 0 || index > num){
+			return; //invalid index 
+		}
+		if (index == 0){
+			addToFront(studentToAdd);
+			return;
+		}
+		if (index == num){
+			addToRear(studentToAdd);
+			return;
+		}
+		StudentInfo current = front;
+		for (int i = 0; i < index - 1; i++){
+			current = current.next;
+		}
+		studentToAdd.next = current.next;
+		current.next = studentToAdd;
+		num++;
 	}
 	
-	
 	public void displayList() {
-		
 		// Start at the front and walk to the rear, printing out each first name.
-		
 		if (num == 0) {
 			System.out.println("\n\nThe list is empty!!!");
 			return;
 		}
-		
 		if (num > 0) {
 			System.out.println("\n\nHere is the list from front to rear!");
 			StudentInfo currentStudent = front;
@@ -49,11 +109,7 @@ public class MyLL {
 				currentStudent = currentStudent.next;
 			}
 			return;
-		}
-		
-		
-		
+		}	
 	}
-	
-
 }
+
